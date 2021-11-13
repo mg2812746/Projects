16e53b54d0c73191a6716b3641d47d9ed7f9a4a2
Miguel Galvez <mikey.galvez1@gmail.com>
2021-11-12 22:30:45 -0800

    Added a throw card to pile functionality

--- Proj/Uno_V7_Rules/main.cpp
+++ Proj/Uno_V7_Rules/main.cpp
@@ -10,7 +10,8 @@
 #include <iostream> //cin,cout,endl
 #include <cstring>  //strcpy, strstr
 #include <iomanip>  //xprecision,fixed,showpoint
-#include <fstream>  //file operations
+#include <fstream>
+#include <valarray>  //file operations
 using namespace std;
 //User Libraries Here
 //Structures
@@ -39,6 +40,7 @@ void define(Deck *);
 void destroy(Deck&,Deck&,Deck&,Deck&);
 void display(Deck *);
 void draw(bool [],short,Uno &,Deck);
+void show(Uno *);
 void wrteRd(Deck &set,Deck &fSet,fstream &out,
         fstream &in);
 void svnCrds(Deck&,Deck&,Deck,bool [],short);
@@ -50,6 +52,8 @@ int main(int argc, char** argv) {
     const short SZE=108;    //Uno Deck Size
     const short PLR=54;     //Player hand size
     bool stat[SZE]; //Status of Deck array
+    bool reverse;   //Status for reverse, true for order player 1 to 
+                    //last player and false for opposite order
     fstream in;     //input file
     fstream out;    //output file
     Deck set;       //Deck of uno cards
@@ -57,13 +61,14 @@ int main(int argc, char** argv) {
     Deck plr1;      //Player 1
     Deck plr2;      //Player 2
     Uno pile;       //Current card on the pile
-    int input;   //user input
+    int input;      //user input
     //Display Uno game header
     cout<<"Welcome to the game of Uno! "<<endl
         <<"Enter 0 to begin, or anything else to "
         <<"end this program. ";
     cin>>input;
     if(input==0){
+        
         //Dynamically Allocate Memory for Deck and Players
         create(set,fSet,SZE);
         create(plr1,plr2,PLR);
@@ -79,12 +84,13 @@ int main(int argc, char** argv) {
         svnCrds(plr1,plr2,fSet,stat,SZE);
         //Draw a card for pile
         draw(stat,SZE,pile,fSet);
+        while(plr1.size!=0&&plr2.size!=0){
         //Show player 1's hands
         cout<<"Player 1's hand: "<<endl;
         display(&plr1);
         //Display card on top of pile
-        cout<<"Card on top of pile: "
-            <<setw(10)<<pile.color<<setw(5)<<pile.value<<endl;
+        cout<<"Card on top of pile: ";
+        show(&pile);
         //Prompt player 1 for an applicable card to throw to the pile
         //Otherwise draw a card
         cout<<"Enter the card # to throw to the pile. "<<endl;
@@ -96,15 +102,91 @@ int main(int argc, char** argv) {
             cin>>input;
         }
         //If card is valid, toss card onto pile
-        if(plr1.all[input].color==pile.color){
-            string temp;
-            int x;
-            
+        if(!strcmp(plr1.all[input].color,pile.color)){
+            //Assign color from player's hand to pile
+            strcpy(pile.color,plr1.all[input].color);
+            //Assign value from player's hand to pile
+            pile.value=plr1.all[input].value;
+            //Remove card from player's hand
+            for(int i=input-1;i<plr1.size;i++){
+            strcpy(plr1.all[i].color,plr1.all[i+1].color); 
+            plr1.all[i].value=plr1.all[i+1].value;
+            }
+            //Decrement plr1's hand size
+            plr1.size--;
+        }else if(plr1.all[input].value==pile.value){
+            //Assign color from player's hand to pile
+            strcpy(pile.color,plr1.all[input].color);
+            //Assign value from player's hand to pile
+            pile.value=plr1.all[input].value;
+            //Remove card from player's hand
+            for(int i=input-1;i<plr1.size;i++){
+            strcpy(plr1.all[i].color,plr1.all[i+1].color); 
+            plr1.all[i].value=plr1.all[i+1].value;
+            }
+            //Decrement plr1's hand size
+            plr1.size--;
+        }else if(plr1.all[input].value>NINE){
+            //Assign color from player's hand to pile
+            strcpy(pile.color,plr1.all[input].color);
+            //Assign value from player's hand to pile
+            pile.value=plr1.all[input].value;
+            //Remove card from player's hand
+            for(int i=input-1;i<plr1.size;i++){
+            strcpy(plr1.all[i].color,plr1.all[i+1].color); 
+            plr1.all[i].value=plr1.all[i+1].value;
+            }
+            plr1.size--;
         }
-        //Otherwise
         //Display player 2's hand
         cout<<"Player 2's hand: "<<endl;   
         display(&plr2);
+        //Display top of pile
+        cout<<"Card on top of pile: ";
+        show(&pile);
+        //Prompt player 1 for an applicable card to throw to the pile
+        //Otherwise draw a card
+        cout<<"Enter the card # to throw to the pile. "<<endl;
+        cin>>input;
+        //Input Validation
+        while(input>plr2.size){
+            cout<<"Error: enter a valid number."<<endl;
+            cout<<"Enter the card # to throw to the pile. "<<endl;
+            cin>>input;
+        }
+        //If card is valid, toss card onto pile
+        if(!strcmp(plr2.all[input].color,pile.color)){
+            //Assign color from player's hand to pile
+            strcpy(pile.color,plr2.all[input].color);
+            //Assign value from player's hand to pile
+            pile.value=plr2.all[input].value;
+            //Remove card from player's hand
+            for(int i=input-1;i<plr2.size;i++){
+            strcpy(plr2.all[i].color,plr2.all[i+1].color); 
+            plr2.all[i].value=plr2.all[i+1].value;
+            }
+            //Decrement plr2's hand size
+            plr2.size--;
+        }else if(plr2.all[input].value==pile.value){
+            //Assign color from player's hand to pile
+            strcpy(pile.color,plr2.all[input].color);
+            //Assign value from player's hand to pile
+            pile.value=plr2.all[input].value;
+            //Decrement plr1's hand size
+            plr2.size--;
+        }else if(plr2.all[input].value>NINE){
+            //Assign color from player's hand to pile
+            strcpy(pile.color,plr2.all[input].color);
+            //Assign value from player's hand to pile
+            pile.value=plr2.all[input].value;
+            //Remove card from player's hand
+            for(int i=input-1;i<plr1.size;i++){
+            strcpy(plr2.all[i].color,plr2.all[i+1].color); 
+            plr2.all[i].value=plr2.all[i+1].value;
+            }
+            plr2.size--;
+        }
+        }
     //Cleanup
     destroy(set,fSet,plr1,plr2);
     }
@@ -204,7 +286,7 @@ void display(Deck *x){
             <<setw(10)<<"Color"<<setw(5)
             <<"Value"<<endl;
     for(int i=0;i<x->size;i++){
-        cout<<setw(6)<<"Card "<<i+1<<": " 
+        cout<<setw(6)<<"Card "<<i<<": " 
             <<setw(10)<<x->all[i].color
             <<setw(5);
             x->all[i].value==SKIP?cout<<"SKIP"<<endl:
@@ -226,6 +308,18 @@ void draw(bool stat[],short SZE,Uno &pile,Deck fSet){
         pile.value=fSet.all[indx].value;
     //Falsify index in bool array
     stat[indx]=0;
+}
+void show(Uno *x){
+    cout<<left<<setw(10)<<"Color"<<setw(5)
+        <<"Value"<<endl;
+    cout<<setw(10)<<x->color
+        <<setw(5);
+        x->value==SKIP?cout<<"SKIP"<<endl:
+        x->value==REVERSE?cout<<"REVERSE"<<endl:
+        x->value==DRAW?cout<<"DRAW +1"<<endl:
+        x->value==WILD?cout<<"WILDCARD"<<endl:
+        x->value==DRAWWILD?cout<<"DRAW+4"<<endl:
+        cout<<x->value<<endl;
 }
 void wrteRd(Deck &set,Deck &fSet,fstream &out,
         fstream &in){
